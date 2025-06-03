2ed8c75e33d543484d8355c79e6e866f54ff085f
{Joao} <jgfreitas.ochoa@gmail.com}>
2025-06-03 17:25:44 -0300

    Sincronizando corretamente

--- start/routes.ts
+++ start/routes.ts
@@ -1,7 +1,8 @@
 import router from '@adonisjs/core/services/router'
 
-router.resource('medicos', () => import('#controllers/medicos_controller')).apiOnly()
 router.resource('pacientes', () => import('#controllers/pacientes_controller')).apiOnly()
+router.resource('medicos', () => import('#controllers/medicos_controller')).apiOnly()
 router.resource('convenios', () => import('#controllers/convenios_controller')).apiOnly()
-router.resource('documentos', () => import('#controllers/documentos_prova_controller')).apiOnly()
-router.resource('tiposanguineos', () => import('#controllers/tipo_sanguineos_controller')).apiOnly()
+router.resource('parentescos', () => import('#controllers/parentescos_controller')).apiOnly()
+router.resource('tipo_sanguineos', () => import('#controllers/tipo_sanguineos_controller')).apiOnly()
+router.resource('documentos_prova', () => import('#controllers/documentos_prova_controller')).apiOnly()
