bdc21e65a05370ff00c820cdb54a85f89bdc0848
MisterJohnsu <jgfreitas.ochoa@gmail.com>
2025-06-06 00:01:01 -0300

    convenio ok

--- app/controllers/convenios_controller.ts
+++ app/controllers/convenios_controller.ts
@@ -2,30 +2,57 @@ import type { HttpContext } from '@adonisjs/core/http'
 import Convenio from '#models/convenio'
 
 export default class ConveniosController {
-  async index({}: HttpContext) {
-    return await Convenio.all()
+  async create({ request, response }: HttpContext) {
+    try {
+      const { data } = request.all()
+
+      if (!data) {
+        return response.badRequest({ message: 'No data provided for creation' })
+      }
+
+      const convenio = await Convenio.create(data)
+
+      return response.created(convenio)
+    } catch (error) {
+      throw error
+    }
   }
 
-  async store({ request }: HttpContext) {
-    const data = request.only(['nm_convenio'])
-    return await Convenio.create(data)
+  async show({ params, response }: HttpContext) {
+    try {
+      const convenio = await Convenio.findOrFail(params.id)
+
+      return response.ok(convenio)
+    } catch (error) {
+      throw error
+    }
   }
 
-  async show({ params }: HttpContext) {
-    return await Convenio.findOrFail(params.id)
-  }
+  async update({ params, request, response }: HttpContext) {
+    try {
+      const convenio = await Convenio.findOrFail(params.id)
+      const { data } = request.all()
 
-  async update({ params, request }: HttpContext) {
-    const convenio = await Convenio.findOrFail(params.id)
-    const data = request.only(['nm_convenio'])
-    convenio.merge(data)
-    await convenio.save()
-    return convenio
+      if (!data) {
+        return response.badRequest({ message: 'No data provided for update' })
+      }
+
+      convenio.merge(data)
+      await convenio.save()
+
+      return response.ok(convenio)
+    } catch (error) {
+      throw error
+    }
   }
 
   async destroy({ params, response }: HttpContext) {
-    const convenio = await Convenio.findOrFail(params.id)
-    await convenio.delete()
-    return response.noContent()
+    try {
+      const convenio = await Convenio.findOrFail(params.id)
+      await convenio.delete()
+      return response.noContent()
+    } catch (error) {
+      throw error
+    }
   }
 }
--- start/routes.ts
+++ start/routes.ts
@@ -1,16 +1,32 @@
-import router from "@adonisjs/core/services/router";
+import router from '@adonisjs/core/services/router'
 
-const PacientesController = () => import("#controllers/pacientes_controller");
-const DocumentosController = () => import("#controllers/documentos_prova_controller");
+const PacientesController = () => import('#controllers/pacientes_controller')
+const DocumentosController = () => import('#controllers/documentos_prova_controller')
+const ConveniosController = () => import('#controllers/convenios_controller')
 
-router.group(() => {
+router
+  .group(() => {
     router.post('/register', [PacientesController, 'create'])
     router.put('/:id', [PacientesController, 'update'])
     router.get('/:id', [PacientesController, 'show'])
     router.delete('/:id', [PacientesController, 'destroy'])
-}).prefix("pacientes")
+  })
+  .prefix('pacientes')
 
-
-router.group(() => {
+router
+  .group(() => {
     router.post('/register', [DocumentosController, 'create'])
-}).prefix("documentos")
+    router.put('/:id', [DocumentosController, 'update'])
+    router.get('/:id', [DocumentosController, 'show'])
+    router.delete('/:id', [DocumentosController, 'destroy'])
+  })
+  .prefix('documentos')
+
+router
+  .group(() => {
+    router.post('/register', [ConveniosController, 'create'])
+    router.put('/:id', [ConveniosController, 'update'])
+    router.get('/:id', [ConveniosController, 'show'])
+    router.delete('/:id', [ConveniosController, 'destroy'])
+  })
+  .prefix('convenios')
